f571704b623e8275df40299a11e443dda8593f73
ayudian12 <70583611+ayudian12@users.noreply.github.com>
2020-09-01 21:46:22 +0700

    Add files via upload

---
+++ AyuDian.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudian;
+
+/**
+ *
+ * @author ASUS A407M
+ */
+public class AyuDian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nama siswa : ayu dian rahmadhani");
+        System.out.println("jenis kelamin : perempuan");
+        System.out.println("alamat : purwodadi jawa timur");
+        System.out.println("jurusan : rekayasa perangkat lunak");
+    }
+    
+}
